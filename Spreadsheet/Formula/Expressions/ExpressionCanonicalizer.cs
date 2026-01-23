cd4315ae80af920458306ea48ba7b65c8de5bad1
Olivia Banks <elegant.lion1334@fastmail.com>
2026-01-22 17:09:00 -0700

    [mid] write expr canonicalizer

---
+++ Spreadsheet/Formula/Expressions/ExpressionCanonicalizer.cs
@@ -0,0 +1,111 @@
+using System.Globalization;
+using System.Text;
+
+namespace Formula.Expressions;
+
+/// <summary>
+///     <para>
+///        Converts an expression into its canonical string form, as defined in PS2.
+///     </para>
+/// </summary>
+public class ExpressionCanonicalizer : IExpressionVisitor
+{
+    /// <summary>
+    ///     <para>
+    ///         The builder used to construct the canonical form string.
+    ///     </para>
+    /// </summary>
+    private readonly StringBuilder _canonicalFormBuilder = new();
+    
+    /// <summary>
+    ///     <para>
+    ///         The canonical form of the expression, as constructed by visiting its nodes.
+    ///     </para>
+    /// </summary>
+    public string CanonicalForm => _canonicalFormBuilder.ToString();
+
+    /// <summary>
+    ///     <para>
+    ///         See <see cref="ExpressionCanonicalizer"/>; this constructor starts the canonicalization process.
+    ///     </para>
+    /// </summary>
+    /// <param name="expression">The expression to canonicalize.</param>
+    public ExpressionCanonicalizer(Expression expression)
+    {
+        expression.Accept(this);
+    }
+
+    /// <inheritdoc />
+    public void Visit(BinaryOpExpression binary)
+    {
+        binary.Left.Accept(this);
+
+        char operatorChar;
+        switch (binary.Op)
+        {
+            case BinaryOpKind.Addition:
+                operatorChar = '+';
+                break;
+
+            case BinaryOpKind.Subtraction:
+                operatorChar = '-';
+                break;
+
+            case BinaryOpKind.Multiplication:
+                operatorChar = '*';
+                break;
+
+            case BinaryOpKind.Division:
+                operatorChar = '/';
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        _canonicalFormBuilder.Append(operatorChar);
+        binary.Right.Accept(this);
+    }
+
+    /// <inheritdoc />
+    public void Visit(CellReferenceExpression cellRef)
+    {
+        var columnIndex = cellRef.ColumnIndex;
+        var rowIndex = cellRef.RowIndex;
+        
+        if (columnIndex < 0 || rowIndex < 0)
+            throw new InvalidOperationException("Cannot canonicalize a cell reference with negative indices.");
+
+        // Convert column index to letters via base-26 representation with 'A' = 0, 'B' = 1, ..., 'Z' = 25.
+        {
+            var columnChars = new List<char>();
+
+            do
+            {
+                var remainder = columnIndex % 26;
+                columnChars.Add((char)('A' + remainder));
+                columnIndex = columnIndex / 26 - 1;
+            } while (columnIndex >= 0);
+
+            columnChars.Reverse();
+            _canonicalFormBuilder.Append(new string(columnChars.ToArray()));
+        }
+
+        // Convert row index to 1-based number.
+        _canonicalFormBuilder.Append(rowIndex + 1);
+    }
+
+    /// <inheritdoc />
+    public void Visit(ConstantExpression constant)
+    {
+        _canonicalFormBuilder.Append(constant.Value.ToString(CultureInfo.InvariantCulture));
+    }
+
+    /// <inheritdoc />
+    public void Visit(ParentheticalExpression parenthetical)
+    {
+        _canonicalFormBuilder.Append('(');
+        parenthetical.Inner.Accept(this);
+        _canonicalFormBuilder.Append(')');
+    }
+}
